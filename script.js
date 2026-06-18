cb888af54143f1c7829d07b7d510fef7b1b022d7
AtomosEnAlmibar <jcmmnbvcxz@gmail.com>
2026-06-18 17:37:08 +0200

    Finished styling all index

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-const myLibrary = [];
+// const myLibrary = [];
 
-function Book() {
-    if (!new.target) {
-        throw Error("You must use the 'new' operator to call the constructor");
-    }
+// function Book() {
+//     if (!new.target) {
+//         throw Error("You must use the 'new' operator to call the constructor");
+//     }
 
-    id = crypto.randomUUID();
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
+//     id = crypto.randomUUID();
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     this.read = read;
+// }
 
-function addBookToLibrary() {
-  // take params, create a book then store it in the array
-}
+// function addBookToLibrary() {
+//   // take params, create a book then store it in the array
+// }
+
+const dialogElem = document.getElementById("dialog");
+const showBtn = document.querySelector(".add-book");
+const closeBtn = document.querySelector(".close");
+
+showBtn.addEventListener("click", () => {
+  dialogElem.showModal();
+});
